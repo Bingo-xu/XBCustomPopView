04bc36543eea5dd09a5193afa00a0ca0faea0c05
xubin <xb624784368@qq.com>
2017-12-22 09:42:53 +0800

    0.0.1

---
+++ XBCustomPopView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XBCustomPopView
+//
+//  Created by xubin on 2017/12/21.
+//  Copyright © 2017年 xubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
